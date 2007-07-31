e64c2bf9ed9d2d7fed05f880a023adc4e8950803
taphorn <taphorn>
2007-07-31 22:48:34 +0000

    Fix for issue [ 1764890 ] cmpi-dns: More specific on package content
    Additionally solved includes in RegisteredProfile provider due to changes
    in the CPMI Header files

--- provider/Linux_DnsRegisteredProfile/CmpiLinux_DnsRegisteredProfileProvider.c
+++ provider/Linux_DnsRegisteredProfile/CmpiLinux_DnsRegisteredProfileProvider.c
@@ -17,6 +17,7 @@
 #include <cmpidt.h>
 #include <cmpift.h>
 #include <cmpimacs.h>
+#include <stdio.h>
 
 #ifdef CMPI_VER_100
 #define Linux_DnsRegisteredProfile_SetInstance Linux_DnsRegisteredProfile_ModifyInstance 
