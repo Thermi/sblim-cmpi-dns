f6c107c0ad70745fe03a9194d12f6d21c288ac83
Jeremy Bongio <jbongio@us.ibm.com>
2009-09-16 01:16:10 -0400

    Corrected the Eclipse Public License URL in the header of each source and header file.

--- provider/Linux_DnsElementConformsToProfile/Linux_DnsElementConformsToProfile.c
+++ provider/Linux_DnsElementConformsToProfile/Linux_DnsElementConformsToProfile.c
@@ -1,12 +1,14 @@
 // ============================================================================
 // Copyright © 2007, International Business Machines
 //
+// Copyright (c) 2006, 2009, International Business Machines
+//
 // THIS FILE IS PROVIDED UNDER THE TERMS OF THE ECLIPSE PUBLIC LICENSE
-// ("AGREEMENT"). ANY USE, REPRODUCTION OR DISTRIBUTION OF THIS FILE
+// ("AGREEMENT"). ANY USE, REPRODUCTION OR DISTRIBUTION OF THIS FILE 
 // CONSTITUTES RECIPIENTS ACCEPTANCE OF THE AGREEMENT.
 //
 // You can obtain a current copy of the Eclipse Public License from
-// http://oss.software.ibm.com/developerworks/opensource/license-cpl.html
+// http://www.opensource.org/licenses/eclipse-1.0.php
 //
 // Authors:             Sam Kannan <samkannan (at) us.ibm.com>
 // ============================================================================
